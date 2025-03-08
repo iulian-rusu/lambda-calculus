88159ef963129adf3fd6932b0848ee2b3ad0225e
Iulian <iulian.rusu999@gmail.com>
2025-03-08 22:00:31 +0200

    Refactor: make lexer stateful

--- src/main/kotlin/lambda/compiler/lexer/Lexer.kt
+++ src/main/kotlin/lambda/compiler/lexer/Lexer.kt
@@ -8,51 +8,12 @@ import lambda.compiler.result.Result
 
 typealias TokenResult = Result<Token>
 
-data object Lexer {
-    fun tokenize(source: String) = tokenize(SourceCursor(source))
+class Lexer(source: String) {
+    private val cursor: SourceCursor = SourceCursor(source)
 
-    private fun tokenize(cursor: SourceCursor): Sequence<TokenResult> = sequence {
-        suspend fun SequenceScope<TokenResult>.yieldToken(
-            kind: TokenKind,
-            span: Span,
-            location: Location
-        ) = yield(
-            Result.Ok(
-                Token(kind, value = cursor[span], span, location)
-            )
-        )
-
-        suspend fun SequenceScope<TokenResult>.yieldError(error: LexicalError) = yield(Result.Error(error))
-
-        suspend fun SequenceScope<TokenResult>.tokenizeCurrentChar(kind: TokenKind) {
-            yieldToken(kind, cursor.currentCharSpan, cursor.location)
-            cursor.advance()
-        }
-
-        suspend fun SequenceScope<TokenResult>.tokenizeIdentifier() {
-            val beginIndex = cursor.index
-            val beginLocation = cursor.location
-            advanceToIdentifierEnd(cursor)
-            yieldToken(
-                TokenKind.IDENT,
-                Span(begin = beginIndex, end = cursor.index),
-                beginLocation
-            )
-        }
-
-        suspend fun SequenceScope<TokenResult>.handleInvalidChar(char: Char) {
-            yieldError(
-                LexicalError.InvalidChar(
-                    char = char,
-                    location = cursor.location,
-                )
-            )
-            cursor.advance()
-        }
-
+    fun tokens(): Sequence<TokenResult> = sequence {
         while (true) {
             val char = cursor.currentChar ?: break
-
             when {
                 char.isIgnored() -> cursor.advance()
                 char == '\\' -> tokenizeCurrentChar(TokenKind.REVERSE_SOLIDUS)
@@ -65,11 +26,42 @@ data object Lexer {
         }
     }
 
+    private suspend fun SequenceScope<TokenResult>.tokenizeCurrentChar(kind: TokenKind) {
+        yieldToken(kind, cursor.currentCharSpan, cursor.location)
+        cursor.advance()
+    }
+
+    private suspend fun SequenceScope<TokenResult>.tokenizeIdentifier() {
+        val beginIndex = cursor.index
+        val beginLocation = cursor.location
+        advanceToIdentifierEnd(cursor)
+        yieldToken(
+            TokenKind.IDENT,
+            Span(begin = beginIndex, end = cursor.index),
+            beginLocation
+        )
+    }
+
     private fun advanceToIdentifierEnd(cursor: SourceCursor) {
         do {
             cursor.advance()
         } while (cursor.currentChar?.isIdentifierContent() == true)
     }
+
+    private suspend fun SequenceScope<TokenResult>.handleInvalidChar(char: Char) {
+        yieldError(
+            LexicalError.InvalidChar(
+                char = char,
+                location = cursor.location,
+            )
+        )
+        cursor.advance()
+    }
+
+    private suspend fun SequenceScope<TokenResult>.yieldToken(kind: TokenKind, span: Span, location: Location) =
+        yield(Result.Ok(Token(kind, value = cursor[span], span, location)))
+
+    private suspend fun SequenceScope<TokenResult>.yieldError(error: LexicalError) = yield(Result.Error(error))
 
     private fun Char.isIgnored() = isWhitespace()
     private fun Char.isIdentifierStart() = isLetter() || this == '_'
